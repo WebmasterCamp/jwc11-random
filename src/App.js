d8fe7f67151917233c03dd6260a930f80e1e74c5
Nutchanon Ploypray <mrearth25@gmail.com>
2019-04-06 01:50:11 +0700

    rand

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClass: 0,
+      team: 0
+    };
+  }
+  animate() {
+    this.setState({ isClass: 1 });
+    const rand = Math.floor(Math.random() * 7) + 1; 
+    this.setState({team: rand})
+    // setTimeout(() => {
+    //   console.log("eath");
+    //   this.setState({ isClass: 1 });
+    // }, 4000);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="App">
+        <img
+          src="./coin.png"
+          alt=""
+          className={(this.state.isClass ? "coin7" : "") + " start"}
+        />
+        <img className="bg" src="./RANDOM.png" alt="" />
+        <button className="btn" onClick={() => this.animate()} />
+      <div>{this.state.team}</div>
+        
       </div>
     );
   }
---
+++ src/genrandom.js
@@ -0,0 +1,45 @@
+const rand = [
+  "{  left: 680px;  top: 50px;}",
+  "{  left: 920px;  top: 405px;}",
+  "{  left: 440px;  top: 405px;}",
+  "{  left: 920px;  top: 190px;}",
+  "{  left: 680px;  top: 540px;}",
+  "{  left: 440px;  top: 190px;}",
+  "{  left: 680px;  top: 50px;}",
+  "{  left: 920px;  top: 405px;}",
+  "{  left: 440px;  top: 405px;}",
+  "{  left: 920px;  top: 190px;}",
+  "{  left: 680px;  top: 540px;}",
+  "{  left: 440px;  top: 190px;}",
+  "{  left: 680px;  top: 50px;}",
+  "{  left: 920px;  top: 405px;}",
+  "{  left: 440px;  top: 405px;}",
+  "{  left: 920px;  top: 190px;}",
+  "{  left: 680px;  top: 540px;}",
+  "{  left: 440px;  top: 190px;}",
+]
+
+console.log("0 %{  left: 680px;  top: 300px;}");
+for (let i =1;i < 51 ;i++){
+  const t =(Math.random() * 1000 ) % 18
+  console.log(i*2+"% "+ `{left: ${parseInt(((Math.random()*1000) % 400))+ 440}px; top: ${parseInt(((Math.random()*1000) % 400))+ 50}px;}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
